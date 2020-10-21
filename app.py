7b91f7729cc90ea1e4a8dc3fb3f073ad2b3930c6
LucasAlmeida-cmd <lucasalmeidafmoraes@gmail.com>
2020-10-20 21:01:05 -0300

    AC4

--- app.py
+++ app.py
@@ -4,20 +4,22 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def ac4():  
-    pro = 1 
-    ant = 0 
+def nao_entre_em_panico():
+    pro = 1
+    ant = 0
     limite = 50
-    f = 0   
+    f = 0
     r = "0, "
     while (f < limite):
-        x = pro 
-        pro = pro + ant 
-        ant = x 
-        f = f + 1  
-        r += str(pro) + ',' 
-    
-    return r 
+        x = pro
+        pro = pro + ant
+        ant = x
+        f = f + 1
+        r += str(pro) + ','
+
+    return r
+
+
 if __name__ == "__main__":
-    port = int(os.envinon.get("PORT", 5000))
-    app.run(host"0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
